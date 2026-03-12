36962ef9b5a0dee743bd4bb262543f24ed642667
Ijtaba Hasan <iijtaba.hasan@gmail.com>
2026-03-12 08:29:07 +0500

    policy remove

--- us_visa/configuration/Mongodb_connection.py
+++ us_visa/configuration/Mongodb_connection.py
@@ -28,7 +28,7 @@ class MongoDBClient:
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
-            # logging.info("MongoDB connection succesfull")
+            # logging.info("MongoDB connection sesfull")
         except Exception as e:
-            # raise USvisaException(e,sys)
+
             raise e
